763af6be20353c89a106a49815926384802342d2
Joakim Holmewi <jh222qr@student.lnu.se>
2015-01-21 21:26:46 +0100

    Created the TextAnalyzer class with the GetNumberOfCapitals method which returns an int of the amount of uppercases in the string.

---
+++ Uppercase/Uppercase/Model/TextAnalyzer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Uppercase.Model
+{
+    public static class TextAnalyzer
+    {
+        public static int GetNumberOfCapitals(string message)
+        {
+            int count = 0;
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (char.IsUpper(message[i]))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+        
+    }
+}
